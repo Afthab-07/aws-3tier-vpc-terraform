854ff65d870ff9a18c898a955c197c418dd49a18
Afthab Ahmed KH <afthabahmedkh@gmail.com>
2025-12-23 15:28:38 +0530

    Add private_subnets variable for database tier

--- terraform/v1/variables.tf
+++ terraform/v1/variables.tf
@@ -24,6 +24,12 @@ variable "public_subnets" {
   default     = ["10.0.1.0/24", "10.0.2.0/24"]
 }
 
+variable "private_subnets" {
+  description = "CIDR blocks for private subnets"
+  type = list(string)
+  default = ["10.0.3.0/24", "10.0.4.0/24"]
+}
+
 variable "instance_type" {
   description = "EC2 instance type"
   type        = string
